59c224d0e5ac152fd35d0df0c2c7c83edda8d2da
Sreyas Rajgounda Patil <sreyaspatil5@gmail.com>
2025-07-11 12:50:06 +0530

    Created docker-setup.sh

---
+++ scripts/docker-setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+docker --version
+
+sudo apt-get install docker-compose -y
+sudo apt-get install docker-compose-plugin -y || echo "Plugin not found, continuing..."
+
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.32.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
+
+echo "Docker & Docker Compose installed successfully!"
