c25779f9caabb32ef40a9319c0bc251e2e58d6eb
Sreyas Rajgounda Patil <sreyaspatil5@gmail.com>
2025-07-30 13:00:21 +0530

    Delete frontend/dist directory

--- frontend/dist/js/script.js
+++
@@ -1,8 +0,0 @@
-// Make an API call using the base URL
-fetch(`${apiBaseURL}/api/greet`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data); // Output API response
-        document.getElementById("greeting").innerText = data.message;
-    })
-    .catch(error => console.error("Error:", error));
